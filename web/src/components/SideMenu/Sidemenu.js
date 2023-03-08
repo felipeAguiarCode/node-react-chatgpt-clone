301dad093ac9921bb8ab5f2209445ddbded26a17
FelipeAguiarCode <felipe.silva.aguiar047@gmail.com>
2023-03-08 01:40:39 -0300

    adjust components from frontend

---
+++ web/src/components/SideMenu/Sidemenu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Sidemenu.css'
+
+const SideMenu = ()=> {
+  return(
+    <aside className='sidemenu'>
+      <div className='sidemenu-button'>
+        <span>+</span>
+        Novo chat
+      </div>
+    </aside>
+  )
+}
+
+export default SideMenu
